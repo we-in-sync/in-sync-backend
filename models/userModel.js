3f625fcf1e709d6554fff34ed8802160f0ce9c9a
Hnq <hancu345@gmail.com>
2025-03-05 15:18:23 +0200

    User model and package.json updated

---
+++ models/userModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema(
+   {
+      username: {
+         type: String,
+         required: [true, "Please provide a username.\n"],
+         unique: true,
+         trim: true,
+         minlength: 3,
+         maxlength: 30,
+         match: /^[a-zA-Z0-9_.]+$/,
+         validate: {
+            validator: function (v) {
+               return !/^\d+$/.test(v) && !/^_+$/.test(v) && !/^\.+$/.test(v);
+            },
+            message:
+               "Username cannot be only numbers, only underscores, or only dots.\n",
+         },
+      },
+      email: {
+         type: String,
+         required: [true, "Please provide your email.\n"],
+         unique: true,
+         trim: true,
+         lowercase: true,
+         validate: [validator.isEmail, "Please provide a valid email.\n"],
+      },
+      password: {
+         type: String,
+         required: [true, "Please provide your password"],
+         minlength: 8,
+         maxlength: 32,
+         select: false,
+         match: [
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,32}$/,
+            "Password must be 8-32 characters and include at least one uppercase letter, one lowercase letter, one number, and one special character.",
+         ],
+      },
+      passwordConfirm: {
+         type: String,
+         required: [true, "Please confirm your password"],
+         select: false,
+         validate: {
+            validator: function (el) {
+               return el === this.password;
+            },
+            message: "Passwords are not the same!",
+         },
+      },
+   },
+   { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -3,6 +3,23 @@ dotenv.config({ path: "./.env" });
 const mongoose = require("mongoose");
 const app = require("./app");
 
+const DB =
+   process.env.NODE_ENV === "production"
+      ? process.env.REMOTE_DB_CONNECTION.replace(
+           "<PASSWORD>",
+           process.env.REMOTE_DB_PASSWORD
+        )
+      : process.env.LOCAL_DB;
+
+mongoose
+   .connect(DB)
+   .then(() => {
+      console.log("Connected successfully to the Database.");
+   })
+   .catch((error) => {
+      console.log(error);
+   });
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(process.env.PORT, () => {
    console.log(`Server is listening...`);
