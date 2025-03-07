f00f1f8287b9383e4e052b74f33df3e0ffdd0ce2
Hnq <hancu345@gmail.com>
2025-03-07 09:02:23 +0200

    new security dependencies

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
 const express = require("express");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const userRouter = require("./routes/userRoutes");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
+
+// Body Parser
+app.use(express.json());
+
+// Helmet
+app.use(helmet());
+
+// Cookie Parser
+app.use(cookieParser());
+
+// Logger
+if (process.env.NODE_ENV === "development") {
+   app.use(morgan("dev"));
+}
+
+if (process.env.NODE_ENV === "production") {
+   // TO-DO Better!
+   const limiter = rateLimit({
+      max: 500,
+      windowMs: 60 * 60 * 1000,
+      message: "Too many requests from this ip, please try again in an hour!",
+   });
+   app.use("/api", limiter);
+}
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Parameter pollution
+//app.use(hpp({}))
+
+app.use("/api/users", userRouter);
+
+app.use(globalErrorHandler);
 
 module.exports = app;
