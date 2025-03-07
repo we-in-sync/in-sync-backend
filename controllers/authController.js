facc6b7c7dc9bfb6b80d2466547cd88b8402270a
Hnq <hancu345@gmail.com>
2025-03-07 13:37:57 +0200

    better signup + jwt implementation in authorization header

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,30 @@ const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 const User = require("./../models/userModel");
 
+const signToken = (payload) => {
+   if (process.env.NODE_ENV == "development") {
+      return jwt.sign({ payload }, process.env.JWT_SECRET, {
+         expiresIn: process.env.JWT_EXPIRESIN_DEV,
+      });
+   }
+};
+
+const createSendToken = (user, statusCode, res) => {
+   const token = signToken(user._id);
+
+   user.password = undefined;
+
+   res.setHeader("Authorization", `Bearer ${token}`);
+
+   res.status(statusCode).json({
+      status: "success",
+      token,
+      data: {
+         user,
+      },
+   });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
    const newUser = await User.create({
       username: req.body.username,
@@ -12,17 +36,5 @@ exports.signup = catchAsync(async (req, res, next) => {
       passwordConfirm: req.body.passwordConfirm,
    });
 
-   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-   });
-
-   newUser.password = undefined;
-
-   res.status(201).json({
-      status: "success",
-      token,
-      data: {
-         newUser,
-      },
-   });
+   createSendToken(newUser, 201, res);
 });
